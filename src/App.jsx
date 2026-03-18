647f7737099c6dde4b3ea46b0aee57b94cf82f44
prasad <prasadkollimala@gmail.com>
2026-03-18 21:18:50 +0530

    Updated component importing using lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./LandingPage";
-import Login from "./Login";
-import Dashboard from "./Dashboard";
-import CoursePlayer from "./CoursePlayer";
-import AdminDashboard from "./AdminDashboard";
-import Explore from "./Explore";
-import Profile from "./Profile";
-import Checkout from "./Checkout";
-import CourseDetail from "./CourseDetail";
-import SuperAdmin from "./SuperAdmin";
+
+// Lazy imports (code splitting)
+const LandingPage = lazy(() => import("./LandingPage"));
+const Login = lazy(() => import("./Login"));
+const Dashboard = lazy(() => import("./Dashboard"));
+const CoursePlayer = lazy(() => import("./CoursePlayer"));
+const AdminDashboard = lazy(() => import("./AdminDashboard"));
+const Explore = lazy(() => import("./Explore"));
+const Profile = lazy(() => import("./Profile"));
+const Checkout = lazy(() => import("./Checkout"));
+const CourseDetail = lazy(() => import("./CourseDetail"));
+const SuperAdmin = lazy(() => import("./SuperAdmin"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/course" element={<CoursePlayer />} />
-        <Route path="/user" element={<AdminDashboard />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/course-detail" element={<CourseDetail />} />
-
-        <Route path="/super-admin" element={<SuperAdmin />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/course" element={<CoursePlayer />} />
+          <Route path="/user" element={<AdminDashboard />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/course-detail" element={<CourseDetail />} />
+          <Route path="/super-admin" element={<SuperAdmin />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
